efce3201c1f590be65c8aca66222cf78c7cd223a
zhuzhaoxuan <zhx60403@outlook.com>
2024-11-26 16:55:57 +0800

    feat: 把页面摘取数据的脚本单独抽离

--- src/index.mjs
+++ src/index.mjs
@@ -1,37 +1,10 @@
 import ora from 'ora'
-import axios from 'axios'
 import inquirer from 'inquirer'
-import * as cheerio from 'cheerio'
 import cloneDeep from 'lodash/cloneDeep.js'
-// import puppeteer from 'puppeteer'
-// import playwright from 'playwright'
 
 import { originUrl, sendChoices } from './base.mjs'
 import sendList from './push_target/index.mjs'
-
-function fetchHTML(url) {
-  try {
-    return axios.get(url).then(({ data }) => data)
-  } catch (error) {
-    console.error(`无法获取 URL：${error}`)
-  }
-}
-
-// 主要内容 .mainbox 榜单 .bangdan
-// TODO: 对主要内容和榜单做区分
-
-const mainClass = '.mainbox'
-const rankClass = '.bangdan'
-const sidebarId = '#sidebar'
-const newPostListClass = '.new-post'
-const articleItemClass = '.article-list'
-
-// const browser = await puppeteer.launch()
-// const page = await browser.newPage()
-// await page.goto(url)
-// const sidebar = await page.locator(sidebarId)
-// const img = await page.screenshot({ path: 'screenshot.png' })
-// await browser.close()
+import pageScript from './page_script.mjs'
 
 let isInited = false
 let pushPlatform = []
@@ -42,48 +15,18 @@ const send = list => {
 }
 
 let oldList = []
-const getAttrList = ['href', 'title']
 
 const filterOldList = list =>
   list.filter(({ href }) => !oldList.some(i => i.href === href))
 
 let filterKeyword = ''
 const filterListByKeyword = list =>
-  list.filter(({ title }) => title && filterKeyword.some(i => title.includes(i)))
-
-const getInformationOfPage = url =>
-  fetchHTML(url).then(html => {
-    const $ = cheerio.load(html)
-    const newPostListSelector = `${newPostListClass}`
-
-    // const newPostList = $(sidebarId).find(newPostListSelector).children()
-    const newPostList = $(newPostListSelector).children()
-
-    if (newPostList.length) {
-      const list = []
-      newPostList.each((_, element) => {
-        element.children.forEach(item => {
-          if (Array.isArray(item.children) && item.children.length) {
-            const listItemElement = item.children[item.children.length - 1]
-
-            if (!listItemElement) return
-            const obj = {}
-            getAttrList.forEach(attrKey => {
-              const val = listItemElement?.attribs?.[attrKey]
-              if (val) obj[attrKey] = val
-            })
-
-            list.push(obj)
-          }
-        })
-      })
-      return list
-    }
-    return []
-  })
+  list.filter(
+    ({ title }) => title && filterKeyword.some(i => title.includes(i))
+  )
 
 const main = () => {
-  getInformationOfPage(originUrl).then(list => {
+  pageScript(originUrl).then(list => {
     let pushList = filterOldList(list)
     if (filterKeyword) pushList = filterListByKeyword(pushList)
 
@@ -115,7 +58,8 @@ const taskOption = [
   {
     type: 'input',
     name: 'keyword',
-    message: '请输入过滤关键字，多个关键字用 | 分开，例如：京东|淘宝|拼多多，默认为空：',
+    message:
+      '请输入过滤关键字，多个关键字用 | 分开，例如：京东|淘宝|拼多多，默认为空：',
     default: ''
   }
 ]
---
+++ src/page_script.mjs
@@ -0,0 +1,65 @@
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+
+// import puppeteer from 'puppeteer'
+// import playwright from 'playwright'
+
+function fetchHTML(url) {
+  try {
+    return axios.get(url).then(({ data }) => data)
+  } catch (error) {
+    console.error(`无法获取 URL：${error}`)
+  }
+}
+
+// const browser = await puppeteer.launch()
+// const page = await browser.newPage()
+// await page.goto(url)
+// const sidebar = await page.locator(sidebarId)
+// const img = await page.screenshot({ path: 'screenshot.png' })
+// await browser.close()
+
+const getInformationOfPage = url => {
+  // 主要内容 .mainbox 榜单 .bangdan
+  // TODO: 对主要内容和榜单做区分
+
+  const mainClass = '.mainbox'
+  const rankClass = '.bangdan'
+  const sidebarId = '#sidebar'
+  const newPostListClass = '.new-post'
+  const articleItemClass = '.article-list'
+
+  const getAttrList = ['href', 'title']
+
+  return fetchHTML(url).then(html => {
+    const $ = cheerio.load(html)
+    const newPostListSelector = `${newPostListClass}`
+
+    // const newPostList = $(sidebarId).find(newPostListSelector).children()
+    const newPostList = $(newPostListSelector).children()
+
+    if (newPostList.length) {
+      const list = []
+      newPostList.each((_, element) => {
+        element.children.forEach(item => {
+          if (Array.isArray(item.children) && item.children.length) {
+            const listItemElement = item.children[item.children.length - 1]
+
+            if (!listItemElement) return
+            const obj = {}
+            getAttrList.forEach(attrKey => {
+              const val = listItemElement?.attribs?.[attrKey]
+              if (val) obj[attrKey] = val
+            })
+
+            list.push(obj)
+          }
+        })
+      })
+      return list
+    }
+    return []
+  })
+}
+
+export default getInformationOfPage
